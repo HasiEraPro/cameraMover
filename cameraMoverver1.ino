d475264d8ea377a64fdc9d39a47000cd0e27a81c
HasiEraPro <hasithaera.pro@gmail.com>
2020-12-22 21:00:55 +0530

    changed pins because Do dont have interrupts

--- cameraMoverver1.ino
+++ cameraMoverver1.ino
@@ -1,12 +1,12 @@
 #include <RCSwitch.h>
-
+#define encoderPin 12
 RCSwitch mySwitch = RCSwitch();
 //////////////Settings/////////////////////
 
  ////----------pins------///////
 //motordriver ibt2
 const uint8_t LPWM = 14; // H-bridge leg 1 ->LPWM
-const uint8_t enL = 12;  // H-bridge enable pin 1 -> L_EN
+const uint8_t enL = 16;  // H-bridge enable pin 1 -> L_EN
 
 const uint8_t RPWM = 5; // H-bridge leg 2 ->RPWM
 const uint8_t enR = 4;  // H-bridge enable pin 2 -> R_EN
@@ -16,28 +16,30 @@ const uint8_t trig = 15; //camera trigger
 
 const int reedSensorPin = A0; //without function, in the past trigger button for manual use
 //wheel encoder GPIO16(used for inerrupts) digitally D0 
-const uint8_t encoderPin = 16  ;
- //////////////
+
+
 
 
 ///////////////////////////////////////////
 
 
-// this are my variable for the speed of the motor. At the moment it start moving at 240
+
 uint16_t mspeed = 0;
 int sensorValue = 0;
-uint16_t encdCount = 0;
-uint16_t currentPWMvalueMotor = 100;
+volatile uint16_t encdCount = 0;
+uint16_t currentPWMvalueMotor = 500;
 
 bool initiateComplete = false;
 uint16_t movingSpeed = 250;
-
+bool isMotorRotates = false;
+bool isEncoderRotated = false;
+unsigned long previousMicros;
 //////////////Function prototype//////////////////////
 
 void motor_clockwise(int speed);
 void initiate();
 void motorStopSlowly();
-
+void ICACHE_RAM_ATTR ISR();
 //////////////////////////////////////////////
 void setup()
 {
@@ -63,18 +65,18 @@ void setup()
 
   Serial.println("start");
 
+    //motor_clockwise(100);
 
+  attachInterrupt(digitalPinToInterrupt(encoderPin), ISR, RISING);
 
-  
-    motor_clockwise(100);
-
-
-  //attachInterrupt(digitalPinToInterrupt(encoderPin), ISR, RISING);
+  initiate();
 
 }
 
 void loop()
 {
+
+
 
      
 
@@ -90,6 +92,7 @@ void loop()
 void motor_clockwise(int speed)
 //move motor clockwise
 {
+  
   //pinMode(enL, HIGH);
   //pinMode(enR, LOW);
   //digitalWrite(LPWM, HIGH);
@@ -103,6 +106,8 @@ void motor_clockwise(int speed)
   }
   //analogWrite(LPWM,mspeed); //pwm value
   digitalWrite(RPWM, LOW);
+
+  isMotorRotates = true;
 }
 
 void readReedContact()
@@ -112,30 +117,22 @@ sensorValue = analogRead(reedSensorPin);
 }
 
 
-ICACHE_RAM_ATTR void ISR()
+void ICACHE_RAM_ATTR ISR()
 {
-
-
-  encdCount++;
-
-}
-
-void takepic()
-// simple set trigger pin to high, wait a short time for camera to focus and take pic and set it to low
-{
-  digitalWrite(trig, HIGH);
+  if (micros() - previousMicros >= 500) {
+    //if(encdCount > 60 )encdCount=0;
+    encdCount++;
+    isEncoderRotated = true;
+    previousMicros = micros();
+  }
+ 
+    
   
-  digitalWrite(trig, LOW);
-}
-
-void StillPhoto()
-{
-
-
-
-
 
 }
+
+
+
 
 void MovingPhoto()
 {
@@ -157,12 +154,13 @@ motor_clockwise(movingSpeed);
 void motorStopSlowly()
 {
 
-for (int i = currentPWMvalueMotor ; i >=0 ;i=i-30 )
+for (int i = currentPWMvalueMotor ; i > 20 ;i=i-50 )
 {
 
 motor_clockwise(i);
-
+delayMicroseconds(1000);
 }
+
 
 Serial.print("m slow stop encode count:");Serial.println(encdCount);
 
@@ -171,21 +169,38 @@ Serial.print("m slow stop encode count:");Serial.println(encdCount);
 void initiate()
 {
 bool isReachedStart = false;
+motor_clockwise(currentPWMvalueMotor);
 
 while(!isReachedStart)
 {
-motor_clockwise(currentPWMvalueMotor);
-sensorValue = analogRead(reedSensorPin);
-if(sensorValue > 1022)
+  yield();
+if(isEncoderRotated)
 {
 
-isReachedStart = true;
+Serial.print("count:= ");Serial.println(encdCount);
+isEncoderRotated = false;
+
+}
+//sensorValue = analogRead(reedSensorPin);
+
+//if(sensorValue > 1022)
+//{
+
+//isReachedStart = true;
+//encdCount = 0;
+
+//}
+
+if(encdCount > 200)
+{
 encdCount = 0;
+isReachedStart = true;
 
 }
 
 
 }
+
 motorStopSlowly();
 
 initiateComplete = true;
